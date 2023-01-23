694af13ef17e5a1e79d61f35910db6c888e59d69
DEO-DECODE <106802468+DEO-DECODE@users.noreply.github.com>
2023-01-23 10:43:27 +0530

    Create First Unique Character in a String(LeetCode).cpp

---
+++ First Unique Character in a String(LeetCode).cpp
@@ -0,0 +1,15 @@
+//First Unique Character in a String(LeetCode)
+#include <bits/stdc++.h>
+using namespace std;
+int firstUniqChar(string s) {
+        unordered_map<char, int> mp;
+        for(int i=0; i<s.size(); ++i){
+            mp[s[i]]++;
+        }
+        for(int i=0; i<s.size(); ++i){
+            if(mp[s[i]]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
