88a5186c52113592c9795d0d3d8e44e7b71a3d8e
DEO-DECODE <106802468+DEO-DECODE@users.noreply.github.com>
2023-02-03 19:16:50 +0530

    Update Sort Colors(LeetCode).cpp

--- Sort Colors(LeetCode).cpp
+++ Sort Colors(LeetCode).cpp
@@ -1,36 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 //
-int main(){
-    int tc;
-    cin>>tc;
-    while (tc--)
-    {
-        int n;
-        cin>>n;
-        vector<int> patients;
-        multimap<int, int> mp;
-        for(int i=0; i<n; ++i){
-            int x;
-            cin>>x;
-            patients.push_back(x);
-            mp.insert({x, i});
-        }
-        sort(patients.rbegin(), patients.rend());
-        // reverse(patients.begin(), patients.end());
-        int cnt=1;
-        vector<int> ans(n);
-        for(int i=0; i<n; ++i){
-            auto itr=mp.find(patients[i]);
-            int indexToInsert=(*itr).second;
-            mp.erase(itr);
-            ans[indexToInsert]=cnt;
-            cnt++;
-        }
-        for(int i=0; i<n; ++i){
-            cout<<ans[i]<<" ";
-        }
-        cout<<endl;
-    }
+void sortColors(vector<int>& nums) {
+        /*
+    The algorithm is based on the fact that all the elements to the left of lo is 0s and all the elements to the
+    right of hi is 2.
+    */
     
-}
+    int lo = 0, hi = nums.size() - 1, mid = 0;
+    while (mid <= hi)
+    {
+        if (nums[mid] == 0)
+        {
+            swap(nums[lo], nums[mid]);
+            lo++, mid++;
+        }
+        else if (nums[mid] == 1)
+            mid++;
+        else if (nums[mid] == 2)
+        {
+            swap(nums[mid], nums[hi]);
+            hi--;
+        }
+    }
+    }
